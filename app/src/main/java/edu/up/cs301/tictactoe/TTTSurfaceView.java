b88a924df53199d61181c5f9754365603cd81784
Kathleen Elisabeth Smith <keons1997@gmail.com>
2016-11-11 17:09:41 -0800

    Initial commit

---
+++ app/src/main/java/edu/up/cs301/tictactoe/TTTSurfaceView.java
@@ -0,0 +1,207 @@
+package edu.up.cs301.tictactoe;
+
+import android.app.Activity;
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Matrix;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.graphics.Point;
+import android.graphics.RectF;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.View;
+import edu.up.cs301.game.util.FlashSurfaceView;
+
+/**
+ * A SurfaceView which allows which an animation to be drawn on it by a
+ * Animator.
+ *
+ * @author Steve Vegdahl
+ * @version 23 September 2016
+ *
+ *
+ */
+public class TTTSurfaceView extends FlashSurfaceView {
+
+    // some constants, which are percentages with respect to the minimum
+    // of the height and the width. All drawing will be done in the "middle
+    // square".
+    //
+    // The divisions both horizontally and vertically within the
+    // playing square are:
+    // - first square starts at 5% and goes to 33%
+    // - second square starts at 36% and goes to 64%
+    // - third square starts at 67& and goes to 95%
+    // There is therefore a 5% border around the edges; each square
+    // is 28% high/wide, and the lines between squares are 3%
+    private final static float BORDER_PERCENT = 5; // size of the border
+    private final static float SQUARE_SIZE_PERCENT = 28; // size of each of our 9 squares
+    private final static float LINE_WIDTH_PERCENT = 3; // width of a tic-tac-toe line
+    private final static float SQUARE_DELTA_PERCENT = SQUARE_SIZE_PERCENT
+            + LINE_WIDTH_PERCENT; // distance from left (or top) edge of square to the next one
+
+    /*
+	 * Instance variables
+	 */
+
+    // the game's state
+    protected TTTState state;
+
+    // the offset from the left and top to the beginning of our "middle square"; one
+    // of these will always be zero
+    protected float hBase;
+    protected float vBase;
+
+    // the size of one edge of our "middle square", or -1 if we have not determined
+    // size
+    protected float fullSquare;
+
+    /**
+     * Constructor for the TTTSurfaceView class.
+     *
+     * @param context - a reference to the activity this animation is run under
+     */
+    public TTTSurfaceView(Context context) {
+        super(context);
+        init();
+    }// ctor
+
+    /**
+     * An alternate constructor for use when a subclass is directly specified
+     * in the layout.
+     *
+     * @param context - a reference to the activity this animation is run under
+     * @param attrs   - set of attributes passed from system
+     */
+    public TTTSurfaceView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init();
+    }// ctor
+
+    /**
+     * Helper-method for the constructors
+     */
+    private void init() {
+        setBackgroundColor(backgroundColor());
+    }// init
+
+
+    public void setState(TTTState state) {
+        this.state = state;
+    }
+
+
+    /**
+     * @return
+     * 		the color to paint the tic-tac-toe lines, and the X's and O's
+     */
+    public int backgroundColor() {
+        return Color.BLUE;
+    }
+
+    /**
+     * callback method, called whenever it's time to redraw
+     * frame
+     *
+     * @param g
+     * 		the canvas to draw on
+     */
+    public void onDraw(Canvas g) {
+
+        // update the variables that relate
+        // to the dimensions of the animation surface
+        updateDimensions(g);
+
+        // paint the TTT-board's horizontal and vertical lines
+        Paint p = new Paint();
+        p.setColor(Color.WHITE);
+
+        g.drawCircle(500,500, 100, p);
+
+
+        g.drawCircle(100,100, 50, p);
+
+        // if we don't have any state, there's nothing more to draw, so return
+        if (state == null) {
+            return;
+        }
+
+        // for each square that has an X or O, draw it on the appropriate
+        // place on the canvas
+        for (int row = 0; row < 3; row++) {
+            for (int col = 0; col < 3; col++) {
+                char result = state.getPiece(row, col); // get piece
+                drawSymbol(g, result, col, row);
+            }
+        }
+    }
+
+    /**
+     * update the instance variables that relate to the drawing surface
+     *
+     * @param g
+     * 		an object that references the drawing surface
+     */
+    private void updateDimensions(Canvas g) {
+
+        // initially, set the height and width to be that of the
+        // drawing surface
+        int width = g.getWidth();
+        int height = g.getHeight();
+
+        // Set the "full square" size to be the minimum of the height and
+        // the width. Depending on which is greater, set either the
+        // horizontal or vertical base to be partway across the screen,
+        // so that the "playing square" is in the middle of the screen on
+        // its long dimension
+        if (width > height) {
+            fullSquare = height;
+            vBase = 0;
+            hBase = (width - height) / (float) 2.0;
+        } else {
+            fullSquare = width;
+            hBase = 0;
+            vBase = (height - width) / (float) 2.0;
+        }
+
+    }
+
+    /**
+     * Draw a symbol (X or O) on the canvas in a particular location
+     *
+     * @param g
+     *            the graphics object on which to draw
+     * @param sym
+     *            the symbol to draw (X or O)
+     * @param col
+     *            the column number of the square on which to draw (0, 1 or 2)
+     * @param col
+     *            the row number of the square on which to draw (0, 1 or 2)
+     */
+    protected void drawSymbol(Canvas g, char sym, int col, int row) {
+
+
+    }
+
+    /**
+     * maps a point from the canvas' pixel coordinates to "square" coordinates
+     *
+     * @param x
+     * 		the x pixel-coordinate
+     * @param y
+     * 		the y pixel-coordinate
+     * @return
+     *		a Point whose components are in the range 0-2, indicating the
+     *		column and row of the corresponding square on the tic-tac-toe
+     * 		board, or null if the point does not correspond to a square
+     */
+    public Point mapPixelToSquare(int x, int y) {
+
+
+        return null;
+    }
+
+
+}
